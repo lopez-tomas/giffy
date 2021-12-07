9b6030aab4c39f04e08eb08ccc36fee9d71ff6a2
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-07 17:33:23 -0300

    feat: minor changes.

--- src/App.js
+++ src/App.js
@@ -9,13 +9,6 @@ function App() {
   return (
     <div className="App">
       <section className="App-content">
-        <button onClick={() => {
-          setKeyword('mapache')
-          setLimit(1) 
-        }}
-        >
-          Change keyword
-        </button>
         <ListOfGifs keyword={keyword} limit={limit} />
       </section>
     </div>
