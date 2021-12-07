7f26685a566429ba625c23f345cde297a13c280e
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-07 16:37:20 -0300

    feat: using Gif functional component to show each GIF.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import { useState, useEffect } from 'react'
 import getGifs from './services/getGifs'
 import './App.css';
 
+import Gif from './components/Gif'
+
 function App() {
   const [gifs, setGifs] = useState([])
 
   useEffect(() => {
-    getGifs({ keyword: 'rick', limit: 25 }).then(gifs => setGifs(gifs))
+    getGifs({ keyword: 'programming', limit: 10 }).then(gifs => setGifs(gifs))
   }, [])
 
   return (
     <div className="App">
       <section className="App-content">
         {
-          gifs.map(singleGif => {
-            return <div id={singleGif.id}>
-              <h4>{singleGif.title}</h4>
-              <img src={singleGif.url} alt={singleGif.title} />
-            </div>
-          })
+          gifs.map(({id, title, url}) =>
+            <Gif
+              key={id}
+              title={title}
+              url={url}
+            />
+          )
         }
       </section>
     </div>
