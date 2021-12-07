e2c5926397190d34902f73b7d1f8f2a006dcd554
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-07 17:10:37 -0300

    feat: using useState() hook to have and set keyword and limit for ListOfGifs component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import { useState } from 'react'
 import ListOfGifs from './components/ListOfGifs'
+import './App.css';
 
 function App() {
+  const [keyword, setKeyword] = useState('panda')
+  const [limit, setLimit] = useState(5)
+
   return (
     <div className="App">
       <section className="App-content">
-        <ListOfGifs keyword={'argentina'} limit={5} />
+        <button onClick={() => {
+          setKeyword('mapache')
+          setLimit(1) 
+        }}
+        >
+          Change keyword
+        </button>
+        <ListOfGifs keyword={keyword} limit={limit} />
       </section>
     </div>
   );
