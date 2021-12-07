eee30376ea454411984be3fb02364c48d18c6aa9
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-07 16:44:45 -0300

    feat: ListOfGifs functional component used.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,19 @@ import { useState, useEffect } from 'react'
 import getGifs from './services/getGifs'
 import './App.css';
 
-import Gif from './components/Gif'
+import ListOfGifs from './components/ListOfGifs'
 
 function App() {
   const [gifs, setGifs] = useState([])
 
   useEffect(() => {
-    getGifs({ keyword: 'programming', limit: 10 }).then(gifs => setGifs(gifs))
+    getGifs({ keyword: 'argentina', limit: 5 }).then(gifs => setGifs(gifs))
   }, [])
 
   return (
     <div className="App">
       <section className="App-content">
-        {
-          gifs.map(({id, title, url}) =>
-            <Gif
-              key={id}
-              title={title}
-              url={url}
-            />
-          )
-        }
+        <ListOfGifs gifs={gifs} />
       </section>
     </div>
   );
