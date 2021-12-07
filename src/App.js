b60fb904f91ce49a5955cfa0589b91d2bf55a744
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-07 16:52:01 -0300

    feat: now ListOfGifs component does all the hard-work.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import { useState, useEffect } from 'react'
-import getGifs from './services/getGifs'
 import './App.css';
-
 import ListOfGifs from './components/ListOfGifs'
 
 function App() {
-  const [gifs, setGifs] = useState([])
-
-  useEffect(() => {
-    getGifs({ keyword: 'argentina', limit: 5 }).then(gifs => setGifs(gifs))
-  }, [])
-
   return (
     <div className="App">
       <section className="App-content">
-        <ListOfGifs gifs={gifs} />
+        <ListOfGifs keyword={'argentina'} limit={5} />
       </section>
     </div>
   );
