c5bd211d0099c5c01563ad6a0c9ca619bf1c5e5e
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-07 16:23:36 -0300

    feat: obtaining id, title and image URL from each fetched GIF.

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -2,10 +2,16 @@ const apiKey = process.env.REACT_APP_GIPHY_API_KEY
 
 export default async function getGifs({ keyword = 'morty', limit = 1 } = {}) {
   const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=${limit}&offset=0&rating=g&lang=en`
+
   const response = await fetch(apiURL)
   const {data = []} = await response.json()
+
   if (Array.isArray(data)) {
-    const gifs = data.map(image => image.images.downsized_medium.url)
+    const gifs = data.map(singleGif => {
+      const { id, title, images } = singleGif
+      const { url } = images.downsized_medium
+      return { id, title, url }
+    })
     return gifs
   }
 }
