879b5292d46b0d3c351ddf200b8fa22d60867ca0
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-07 23:40:03 -0300

    feat: minor changes.

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -3,7 +3,7 @@ import './Gif.css'
 
 const Gif = ({ id, title, url }) => {
 	return (
-		<a href={`#${id}`}className="Gif">
+		<a href={`#${id}`} className="Gif">
       <h4>{title}</h4>
       <img src={url} alt={title} />
     </a>
