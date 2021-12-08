657923ffe5deb71d0a1e764c21407625479e8436
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-07 23:20:34 -0300

    feat: only receiving keyword as a parameter and limit defined into function.

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,6 +1,7 @@
 const apiKey = process.env.REACT_APP_GIPHY_API_KEY
 
-export default async function getGifs({ keyword = 'morty', limit = 1 } = {}) {
+export default async function getGifs({ keyword = 'morty' } = {}) {
+  const limit = 10;
   const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=${limit}&offset=0&rating=g&lang=en`
 
   const response = await fetch(apiURL)
