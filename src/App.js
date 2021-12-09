9098b69101e13708a5e8966232d9f1d0039cd2d8
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-09 15:31:58 -0300

    feat: importing & using StaticContext & GifsContextProvider.

--- src/App.js
+++ src/App.js
@@ -4,28 +4,39 @@ import './App.css';
 import Home from './pages/Home/index'
 import SearchResults from './pages/SearchResults/index'
 import Detail from './pages/Detail/index'
+import StaticContext from './context/StaticContext'
+import { GifsContextProvider } from './context/GifsContext'
 
 function App() {
   return (
-    <div className="App">
-      <section className="App-content">
-        <Link to="/">
+    <StaticContext.Provider value={
+      {
+        name: 'midudev',
+        subscribe: true
+      }
+    }>
+      <div className="App">
+        <section className="App-content">
+          <Link to="/">
           <img className="App-logo" alt="Giffy logo" src="/logo.png" />
-        </Link>
-        <Route
-          path="/"
-          component={Home}
-        />
-        <Route
-          path="/search/:keyword"
-          component={SearchResults}
-        />
-        <Route
-          path="/gif/:id"
-          component={Detail}
-        />
-      </section>
-    </div>
+          </Link>
+          <GifsContextProvider>
+            <Route
+              path="/"
+              component={Home}
+            />
+            <Route
+              path="/search/:keyword"
+              component={SearchResults}
+            />
+            <Route
+              path="/gif/:id"
+              component={Detail}
+            />
+          </GifsContextProvider>
+        </section>
+      </div>
+    </StaticContext.Provider>
   );
 }
 
