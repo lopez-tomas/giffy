d81f71ccc82dbe7018e7cd8b36234d5f901bf62c
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-09 13:24:40 -0300

    feat: minor changes to apply useGifs in Home page. If keyword is not sent as a parameter we are getting 'lastKeyword' from localStorage to make used of it in this case and if 'lastKeyword' doensn't exist we use 'random' as keyword. When we get GIFs, we save this keyword into localStorage.

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -1,16 +1,21 @@
 import { useState, useEffect } from 'react'
 import getGifs from '../services/getGifs'
 
-const useGifs = ({ keyword }) => {
+const useGifs = ({ keyword } = { keyword: null }) => {
 	const [loading, setLoading] = useState(false)
 	const [gifs, setGifs] = useState([])
 
 	useEffect(() => {
 		setLoading(true)
-		getGifs({ keyword })
+		// we recover keyword from localStorage
+		const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'random'
+
+		getGifs({ keyword: keywordToUse })
 			.then(gifs => {
 				setGifs(gifs)
 				setLoading(false)
+				// we save keyword in localStorage
+				localStorage.setItem('lastKeyword', keyword)
 			})
 	}, [keyword])
 
