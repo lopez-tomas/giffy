89285636e5c8299dd694439f5ce7202ec986aa0d
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-10 23:03:36 -0300

    feat: obtaining GIF and displaying it with Gif component.

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -2,12 +2,20 @@ import React, { useContext } from 'react'
 import GifsContext from '../../context/GifsContext'
 import './styles.css'
 
+import Gif from '../../components/Gif'
+
 const Detail = ({ params }) => {
 	const { gifs } = useContext(GifsContext)
 	console.log(gifs)
 
+	const gif = gifs.find(singleGif =>
+		singleGif.id === params.id
+	)
+
+	console.log(gif)
+
 	return (
-		<h1>GIF with id {params.id}</h1>
+		<Gif {...gif} />
 	)
 }
 
