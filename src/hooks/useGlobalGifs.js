ba7df720c8139c91e1fa77f2603a62f269b28c8c
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-10 23:13:36 -0300

    feat: new hook added: useGlobalGifs() - It uses GifsContext context and returns GIFs from global state.

---
+++ src/hooks/useGlobalGifs.js
@@ -0,0 +1,10 @@
+import { useContext } from 'react'
+import GifsContext from '../context/GifsContext'
+
+const useGlobalGifs = () => {
+	const { gifs } = useContext(GifsContext)
+
+	return gifs
+}
+
+export default useGlobalGifs
