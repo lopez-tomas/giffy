4e4d91c0c08bcc08d9f802401c7617cf2435ce90
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-10 23:16:14 -0300

    feat: minor changes.

--- src/hooks/useGlobalGifs.js
+++ src/hooks/useGlobalGifs.js
@@ -2,9 +2,7 @@ import { useContext } from 'react'
 import GifsContext from '../context/GifsContext'
 
 const useGlobalGifs = () => {
-	const { gifs } = useContext(GifsContext)
-
-	return gifs
+	return useContext(GifsContext).gifs
 }
 
 export default useGlobalGifs
