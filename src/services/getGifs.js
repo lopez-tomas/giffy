8ff8ca32df2d3b39c6d86dc7146e30f7c92e7691
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-12 20:28:47 -0300

    feat: importing and using API_KEY.

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,8 +1,8 @@
-const apiKey = process.env.REACT_APP_GIPHY_API_KEY
+import { API_KEY } from './settings'
 
 export default async function getGifs({ keyword = 'morty' } = {}) {
   const limit = 10;
-  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=${limit}&offset=0&rating=g&lang=en`
+  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=0&rating=g&lang=en`
 
   const response = await fetch(apiURL)
   const {data = []} = await response.json()
