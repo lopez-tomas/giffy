16713645e92c8adfa5f8bf32f7dda549eebfedeb
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-14 14:32:40 -0300

    feat: new effect created to manage next page of GIFs, minor changes add to make it work.

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -2,14 +2,19 @@ import { useState, useEffect, useContext } from 'react'
 import getGifs from 'services/getGifs'
 import GifsContext from 'context/GifsContext'
 
+const INITIAL_PAGE = 0
+
 const useGifs = ({ keyword } = { keyword: null }) => {
 	const [loading, setLoading] = useState(false)
+	const [loadingNextPage, setLoadingNextPage] = useState(false)
+	const [page, setPage] = useState(INITIAL_PAGE)
 	const { gifs, setGifs } = useContext(GifsContext)
+
+	// we recover keyword from localStorage
+	const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'random'
 
 	useEffect(() => {
 		setLoading(true)
-		// we recover keyword from localStorage
-		const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'random'
 
 		getGifs({ keyword: keywordToUse })
 			.then(gifs => {
@@ -18,9 +23,21 @@ const useGifs = ({ keyword } = { keyword: null }) => {
 				// we save keyword in localStorage
 				localStorage.setItem('lastKeyword', keyword)
 			})
-	}, [keyword, setGifs])
+	}, [keyword, keywordToUse, setGifs])
 
-	return { loading, gifs }
+	useEffect(() => {
+		if (page == INITIAL_PAGE) return
+
+		setLoadingNextPage(true)
+
+		getGifs({ keyword: keywordToUse, page })
+			.then(nextGifs => {
+				setGifs(prevGifs => prevGifs.concat(nextGifs))
+				setLoadingNextPage(false)
+			})
+	}, [page, keywordToUse])
+
+	return { loading, loadingNextPage, gifs, setPage }
 }
 
 export default useGifs
