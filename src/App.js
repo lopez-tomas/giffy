a1e836af9180d3f807b0d212eb1cbbdf44eeb3e6
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-16 13:44:02 -0300

    fix: minor change.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from 'pages/Home/index'
 import SearchResults from 'pages/SearchResults/index'
 import Detail from 'pages/Detail/index'
 import StaticContext from 'context/StaticContext'
-import { GifsContextProvider } from 'context/GifsContext/'
+import { GifsContextProvider } from 'context/GifsContext'
 
 function App() {
   return (
