ab887c6fb67ccadb0eb28d645a96292e7ed33e59
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-16 12:55:47 -0300

    feat: useEffect() & useRef() imported and used. externalRef created for DIV tag with ID=visor. useNearScreen imported sending my externalRef if page isn't loading and receiving a boolean.

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,12 +1,22 @@
+import { useEffect, useRef } from 'react'
 import useGifs from 'hooks/useGifs'
+import useNearScreen from 'hooks/useNearScreen'
 import Spinner from 'components/Spinner/index'
 import ListOfGifs from 'components/ListOfGifs/index'
 
 const SearchResults = ({ params }) => {
 	const { keyword } = params
 	const { loading, gifs, setPage } = useGifs({ keyword })
+	const externalRef = useRef()
+	const { isNearScreen } = useNearScreen({ externalRef: loading ? null : externalRef })
 
-	const handleNextPage = () => setPage(prevPage => prevPage + 1)
+	//const handleNextPage = () => setPage(prevPage => prevPage + 1)
+
+	const handleNextPage = () => console.log('next page')
+
+	useEffect(() => {
+		if (isNearScreen) handleNextPage()
+	})
 
 	return (
 		<>
@@ -17,9 +27,9 @@ const SearchResults = ({ params }) => {
 						{decodeURI(keyword)}
 					</h3>
 					<ListOfGifs gifs={gifs} />
+					<div id="visor" ref={externalRef}></div>
 				</>
 			}
-			<button onClick={handleNextPage}>Get next page</button>
 		</>
 	)
 }
