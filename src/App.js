fcd771fc6fba45535e7beda08fb0f02efa15e887
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-16 17:49:48 -0300

    feat: lazy and Suspense component add to Home page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React, { Suspense } from 'react'
 import { Route, Link } from 'wouter'
 import './App.css';
 
-import Home from 'pages/Home/index'
 import SearchResults from 'pages/SearchResults/index'
 import Detail from 'pages/Detail/index'
 import StaticContext from 'context/StaticContext'
 import { GifsContextProvider } from 'context/GifsContext'
+
+const HomePage = React.lazy(() => import('pages/Home'))
 
 function App() {
   return (
@@ -16,25 +18,28 @@ function App() {
       }
     }>
       <div className="App">
-        <section className="App-content">
-          <Link to="/">
-          <img className="App-logo" alt="Giffy logo" src="/logo.png" />
-          </Link>
-          <GifsContextProvider>
-            <Route
-              path="/"
-              component={Home}
-            />
-            <Route
-              path="/search/:keyword"
-              component={SearchResults}
-            />
-            <Route
-              path="/gif/:id"
-              component={Detail}
-            />
-          </GifsContextProvider>
-        </section>
+        <Suspense fallback={null}>
+          <section className="App-content">
+            <Link to="/">
+            <img className="App-logo" alt="Giffy logo" src="/logo.png" />
+            </Link>
+
+            <GifsContextProvider>
+              <Route
+                path="/"
+                component={HomePage}
+              />
+              <Route
+                path="/search/:keyword"
+                component={SearchResults}
+              />
+              <Route
+                path="/gif/:id"
+                component={Detail}
+              />
+            </GifsContextProvider>
+            </section>
+        </Suspense>
       </div>
     </StaticContext.Provider>
   );
