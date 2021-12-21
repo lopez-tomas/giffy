d5fef7991eb0801c499677bab88afe6034845c15
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-21 17:18:58 -0300

    feat: Helmet component used to change title and meta description tags in Error404 component.

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet'
 import useSingleGif from 'hooks/useSingleGif'
 import Gif from 'components/Gif'
 
@@ -7,6 +8,10 @@ const Error404 = () => {
 
 	return (
 		<div>
+			<Helmet>
+				<title>Error 404 | Giffy</title>
+				<meta name="description" content="Error 404" /> 
+			</Helmet>
 			<h1 className="App-error">ERROR 404 :(</h1>
 			<Gif {...gif} />
 		</div>
