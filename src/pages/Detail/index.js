8c5e544c5203a63f9d1856c09ca38f43ae48abf8
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-21 15:49:29 -0300

    feat: using new hook useSingleGif() insted of useGlobalGifs(). Redirect component imported from wouter to redirect to Error 404 page. Obtaining gif, isLoading & isError from useSingleGif() of actual given GIF's id. Conditional rendering used with isLoading to return Spinner component, with isError to redirect to Error404 page.

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,17 +1,16 @@
-import useGlobalGifs from 'hooks/useGlobalGifs'
+import useSingleGif from 'hooks/useSingleGif'
 import './styles.css'
 
+import { Redirect } from 'wouter'
 import Gif from 'components/Gif'
+import Spinner from 'components/Spinner'
 
 const Detail = ({ params }) => {
-	const gifs = useGlobalGifs()
-	console.log(gifs)
+	const { gif, isLoading, isError } = useSingleGif({ id: params.id })
 
-	const gif = gifs.find(singleGif =>
-		singleGif.id === params.id
-	)
-
-	console.log(gif)
+	if (isLoading) return <Spinner />
+	if (isError) return <Redirect to="/404" />
+	if (!gif) return null
 
 	return (
 		<div>
