6846fcab0fda0d113a3e354bc37e7a51ee9b1dd9
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-21 16:46:42 -0300

    feat: using Helmet component imported from react-helmet to set title tag when loading and when results are ready.

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,5 +1,5 @@
 import useSingleGif from 'hooks/useSingleGif'
-import useSEO from 'hooks/useSEO'
+import { Helmet } from 'react-helmet'
 import './styles.css'
 
 import { Redirect } from 'wouter'
@@ -9,14 +9,26 @@ import Spinner from 'components/Spinner'
 const Detail = ({ params }) => {
 	const { gif, isLoading, isError } = useSingleGif({ id: params.id })
 	const title = gif ? gif.title : ''
-	useSEO({ title, description: `Detail of ${title}` })
 
-	if (isLoading) return <Spinner />
+	if (isLoading) {
+		return (
+			<>
+				<Helmet>
+					<title>Loading...</title>
+				</Helmet>
+				<Spinner />
+			</>
+		)
+	}
+
 	if (isError) return <Redirect to="/404" />
 	if (!gif) return null
 
 	return (
 		<div>
+			<Helmet>
+				<title>{title} | Giffy</title>
+			</Helmet>
 			<h3 className="App-title">{gif.title}</h3>
 			<Gif {...gif} />
 		</div>
